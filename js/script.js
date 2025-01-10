c10ea93e816009c22a61e0efe1a4ad085d0ed7be
Luiz Eduardo <luiz.sinx02@gmail.com>
2025-01-10 02:39:06 -0300

    Melhorando pagina mim

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function sobre_mim_html(){
     
     const html = `
       <div class="header">
-      <img class="header-img" src="imagens\\76561198162290882.png" width="150">
+      <img class="header-img" src="https://storage.googleapis.com/basedosdados-website-images/account/4745.jpeg?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=website-backend%40basedosdados-dev.iam.gserviceaccount.com%2F20250110%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250110T041839Z&X-Goog-Expires=604800&X-Goog-SignedHeaders=host&X-Goog-Signature=966b324a64e45e9757c9375165f8ba16349e0897778731f53985696c1754be9de38042f05e7565083846271090e7539548e5d9146203cb85773ce455d61dff045e162c930229a41658183f5e50f77eaea3d106f81379ef8e76de75d11c8cdd4fa3047effcc63cbd155cf7109d9a3f3942e3fdccfcbabe2e1fbbcd40c48b3a3aae8d94210e0507fbf7db367181916e5ccb0869c088d5c14b9b5edebb62a5dfcc062fa8155b2a5f6377f00a7983be7f22298346f329280aea3b2677b74dea1f09c5639890ec743bbb8f5bfa0310f1468812ca50fb2aa8f65aabc052ae23f3225647eab2885e44960f6c238b16f7c515d62398c2a1c73167ff71ecd9a0f447e7289" width="150">
     <h1>Luiz Eduardo - Engenheiro de Dados</h1>
       </div>
     <p class="texto">
@@ -41,10 +41,10 @@ function sobre_mim_html(){
 
         <p class="texto">Sinta se livre para entrar em contato caso sinta que eu possa te ajudar.</p>
 
-    <footer class="footer">
+    <div class="footer">
     <a href="https://github.com/Winzen" target=_blank><img class="github_icon" src="imagens\\github.png"></a>
     <a href="https://www.linkedin.com/in/luiz-winzen/" target=_blank><img class="github_icon" src="imagens\\linkedin.png"></a>
-    </footer>` 
+    </div>` 
 
     return html
 }
